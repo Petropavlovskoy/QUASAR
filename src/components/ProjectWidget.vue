35016b3848904914070f222d50a153bbc3bfa672
Sergio <sergio033032@gmail.com>
2021-08-16 00:52:22 +0300

    test 3
    test 3

---
+++ src/components/ProjectWidget.vue
@@ -0,0 +1,24 @@
+<template>
+  <q-card class="my-card position-relative" flat bordered>
+    <q-img
+      src="https://sakhexpo.ru/img/6439afe1d64afda2927c12ba9feb781bbb69.jpg"
+    />
+    <q-badge label="Сюреализм" color="red" rounded floating />
+
+    <q-card-section>
+      <div class="text-overline text-orange-9">Высшее качество</div>
+      <div class="text-h5 q-mt-sm q-mb-xs">Сайты на заказ</div>
+      <div class="text-caption text-grey">
+Создаем интерфейсы аналитических систем, мобильные приложения и цифровые сервисы. Cоздаем удобные цифровые сервисы. Продуманный UX.</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script>
+export default {
+  // name: 'ComponentName',
+  data () {
+    return {}
+  }
+}
+</script>
--- src/pages/Projects.vue
+++ src/pages/Projects.vue
@@ -1,11 +1,20 @@
 <template>
-  <q-page padding>
-<div class="text-lg">Projects Page</div>
+  <q-page>
+    <div class="p-4">
+      <div class="text-3xl font-bold pb-4">
+        Hello
+      </div>
+      <div class="project-widget-list space-y-5">
+        <ProjectWidget />
+      </div>
+    </div>
   </q-page>
 </template>
 
 <script>
+import ProjectWidget from "components/ProjectWidget";
 export default {
+  components: {ProjectWidget}
   // name: 'PageName',
 }
 </script>
